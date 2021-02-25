11c721e3f761777cecbd1b015231d0612d50d4d3
Danny Gitelman <55748709+gitelmandanny@users.noreply.github.com>
2021-02-25 11:24:35 +0200

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -9,9 +9,9 @@ while flag == str('y'):
 
     try:
         while True:
-            flag = str(input(print("do you want to play again ? y\\n")))
+            flag = str(input(print("do you want to play again ? y\\n")))  # all inputs are already str, no need to cast it. no need to print too. just in=input("what is your name")
 
-            if flag == str('n'):
+            if flag == str('n'): # want a challenge? make it support yes no aswell
                 break
             elif flag == str('y'):
                 break
