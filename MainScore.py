c1b5b942b7d1a48f2306685e7ace8781d252cb3e
OrenSultan <46323571+OrenSultan@users.noreply.github.com>
2021-04-03 15:36:54 +0300

    mainscore done

---
+++ MainScore.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    if os.path.exists("scores.txt") and (os.path.getsize("scores.txt") != 0):
+        with open('scores.txt', 'r') as f:
+            return render_template('score.html', score=f.read())
+    else:
+        # file error
+        return render_template('error.html')
+
+app.run()
