c270b8ec49bc488848734890390c04426ab94c50
OrenSultan <46323571+OrenSultan@users.noreply.github.com>
2021-04-03 14:46:39 +0300

    fix review and master

--- GuessGame.py
+++ GuessGame.py
@@ -15,9 +15,9 @@ def get_guess_from_user (difficulty):
         try:
             bet = int(input(print(f"please place your bet: 0 to {difficulty}")))
 
-            if (bet > 0) and (bet < (difficulty+1)):  #  check if in range
+            if (bet > 0) and (bet < (difficulty+1)):# check if in range
 
-                return bet               # getting a guess number from user
+                return bet               #getting a guess number from user
             else:
                 print ("bad choice ")
                 error = True
@@ -39,24 +39,8 @@ def play_game(level):
 
     utils.screen_cleaner()
     print("welcome to Guess Game - ")
-<<<<<<< HEAD
-    error = True
-    levels=int(100)
-    # what if additional 100 levels will be added? make more generic solution-done
-    if level == 1:
-        difficulty = levels/5
-    elif level == 2:
-        difficulty = levels/4
-    elif level == 3:
-        difficulty = levels/3
-    elif level == 4:
-        difficulty = levels/2
-    elif level == 5:
-        difficulty = levels
-
-=======
     span = 100
-    error = True 
+    error = True
     # what if additional 100 levels will be added? make more generic solution-done
     if level == 1:
         difficulty = span/5
@@ -68,7 +52,6 @@ def play_game(level):
         difficulty = 4*span/5
     elif level == 5:
         difficulty = span
->>>>>>> review-fixes
 
     secret = int(generate_number(difficulty))
     bet = int(get_guess_from_user(difficulty))
@@ -78,28 +61,15 @@ def play_game(level):
         win = True
     else:
         print(f"the number is  {secret} maybe next time!! ")
-<<<<<<< HEAD
-
-
-#manual playing
-"""
-difficulty=2 #manual playing
-=======
         win = False
     print(trofies)
     return win
 
 
 """difficulty=2 #manual playing
->>>>>>> review-fixes
 flag = 'y'
-
 while flag == str('y'):
-<<<<<<< HEAD
-    play_game(2)
-=======
     play_game(difficulty)
->>>>>>> review-fixes
 
     try:
         while True:
