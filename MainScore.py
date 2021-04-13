f06fff3cf86cdbf1528ca16b6cacc1fd140c798b
OrenSultan <46323571+OrenSultan@users.noreply.github.com>
2021-04-13 10:49:36 +0300

    scores works

--- MainScore.py
+++ MainScore.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    if os.path.exists("Scores.txt") and (os.path.getsize("Scores.txt") != 0):
-        with open('score.html.txt', 'r') as f:
-            return render_template('content.html', score=f.read())
+    if os.path.exists("scores.txt") and (os.path.getsize("scores.txt") != 0):
+        with open('scores.txt', 'r') as f:
+            return render_template('score.html', score=f.read())
     else:
         # file error
         return render_template('error.html')
