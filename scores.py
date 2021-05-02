61c1db991d5f53f14c4cefa9c6b96e170ff3d4f4
OrenSultan <46323571+OrenSultan@users.noreply.github.com>
2021-05-02 20:03:25 +0300

    test and scores confirmed

--- scores.py
+++ scores.py
@@ -11,12 +11,12 @@ def add_score(difficulty, score=0):
 
     file_name = 'scores.txt'
     if os.path.isfile(f"{file_name}"):
-        my_file = open(f"{cwd}\{file_name}", 'r+')
+        my_file = open(f"{cwd}\\score\\{file_name}", 'r+')
         current_score = my_file.read()
         my_file.seek(0)
         score = int(POINTS_OF_WINNING) + int(current_score)
     else:
-        my_file = open(f"{cwd}\{file_name}", "w+")
+        my_file = open(f"{cwd}\\score\\{file_name}", "w+")
         my_file.seek(0)
         my_file.write('0')
         score = int(POINTS_OF_WINNING)
--- tests/e2e.py
+++ tests/e2e.py
@@ -6,14 +6,26 @@ from selenium.webdriver.support.wait import WebDriverWait
 def test_scores_service(url):
     scorepage = webdriver.Chrome(executable_path="C:/Users/user/.selenium/chromedriver.exe")
     scorepage.get(f"{url}")
+    """scorepage.get("http://127.0.0.1:5000/")"""
     scorepage.implicitly_wait(5)
-    a = scorepage.find_element_by_id("score").text
-    if  1<int(a)<1000 :
+    a = scorepage.find_element_by_css_selector("#score").text
+    if 1 < int(a) < 1000:
         return True
-    else :
+    else:
         return False
 
-def main_function():
-    url ='http://127.0.0.1:5000/'
-    a=test_scores_service(url)
-    print(a)
+
+
+def main_function(url):
+
+    calltest = test_scores_service(url)
+    if calltest is True:
+        return 0
+    else:
+        return -1
+
+
+
+score_page ='http://127.0.0.1:5000/'
+OS_exit_code = main_function(score_page)
+print(OS_exit_code)
