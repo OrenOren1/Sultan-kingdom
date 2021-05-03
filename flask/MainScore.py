4373bad271b965ac6e84ac2cc348809be0cedd55
OrenSultan <46323571+OrenSultan@users.noreply.github.com>
2021-05-03 09:11:33 +0300

    docker file add

--- flask/MainScore.py
+++ flask/MainScore.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    if os.path.exists("../score/scores.txt") and (os.path.getsize("../score/scores.txt") != 0):
-        with open('../score/scores.txt', 'r') as f:
+    if os.path.exists("../flask/scores.txt") and (os.path.getsize("../flask/scores.txt") != 0):
+        with open('../flask/scores.txt', 'r') as f:
             return render_template('score.html', score=f.read())
     else:
         # file error
