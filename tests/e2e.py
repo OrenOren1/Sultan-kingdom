ff8531822ef665d43a2034ca1cd8b1a441786cd2
OrenOren1 <46323571+OrenOren1@users.noreply.github.com>
2021-05-03 23:24:51 +0300

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 def test_scores_service(url):
     scorepage = webdriver.Chrome(executable_path="C:/Users/user/.selenium/chromedriver.exe")
     scorepage.get(f"{url}")
-    """scorepage.get("http://127.0.0.1:5000/")"""
+    
     scorepage.implicitly_wait(5)
     a = scorepage.find_element_by_css_selector("#score").text
     if 1 < int(a) < 1000:
@@ -26,6 +26,6 @@ def main_function(url):
 
 
 
-score_page ='http://127.0.0.1:5000/'
+score_page ='http://127.0.0.1:80/'
 OS_exit_code = main_function(score_page)
 print(OS_exit_code)
